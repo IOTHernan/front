c7ad817087a94d5588254a3e3e3e1e733c5ab5d5
Che Tester <che.tester.ar@gmail.com>
2024-03-04 21:00:48 -0300

    15

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { LoginComponent } from './components/login/login.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { AcercadeComponent } from './components/acercade/acercade.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { ProyectosComponent } from './components/proyectos/proyectos.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { PortfolioComponent } from './components/portfolio/portfolio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LoginComponent,
+    FooterComponent,
+    AcercadeComponent,
+    EducacionComponent,
+    ExperienciaComponent,
+    ProyectosComponent,
+    SkillsComponent,
+    PageNotFoundComponent,
+    PortfolioComponent
   ],
   imports: [
     BrowserModule,
